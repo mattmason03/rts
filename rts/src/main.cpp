2376fba44b5f6269ca145e8ae3155f68d75e9969
matt.mason03@gmail.com <matt.mason03@gmail.com>
2016-04-21 21:16:55 -0700

    gl line stuff

--- rts/src/main.cpp
+++ rts/src/main.cpp
@@ -48,10 +48,37 @@ void setExeWorkingDir(char** argv){
 	*lastDelim = delim;
 }
 
+void drawHollowCircle(GLfloat x, GLfloat y, GLfloat radius, GLfloat rot, int lineAmount = 6) {
+	int i;
+
+						  //GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * glm::pi<float>();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3f(0.5f, 0.0f, 1.0f);
+	for (i = 0; i <= lineAmount; i++) {
+		glVertex2f(
+			x + (radius * cos(i *  twicePi / lineAmount)),
+			y + (radius* sin(i * twicePi / lineAmount))
+			);
+	}
+	glEnd();
+
+	glBegin(GL_LINES);
+	glVertex2f(x, y);
+	float rad = radius * 1.2f;
+	glVertex2f(x + rad * cos(rot), y + rad * sin(rot));
+	glEnd();
+}
+
 class TestGame : public Game {
-	virtual void Update(Game::Duration gameTime, Game::Duration timeStep) {
+	void Update(Game::Duration gameTime, Game::Duration timeStep) override {
 		if (std::chrono::duration_cast<std::chrono::seconds>(gameTime).count() > 3)
 			End();
+	}
+
+	void Render(Game::Duration gameTime) override {
+
 	}
 };
 
@@ -60,57 +87,67 @@ int main(int argc, char** argv)
 	TestGame game;
 	game.renderStep = Game::Millis(50);
 	game.updateStep = Game::Millis(100);
-	game.Play();
-	//setExeWorkingDir(argv);
+	setExeWorkingDir(argv);
 
-	//GLFWwindow* window;
+	GLFWwindow* window;
 
-	//glfwSetErrorCallback(error_callback);
-
-
-	//if (!glfwInit())
-	//	exit(EXIT_FAILURE);
-	//window = glfwCreateWindow(640, 640, "Simple example", NULL, NULL);
-	//if (!window)
-	//{
-	//	glfwTerminate();
-	//	exit(EXIT_FAILURE);
-	//}
-	//glfwMakeContextCurrent(window);
-	//glfwSwapInterval(1);
-	//glfwSetKeyCallback(window, key_callback);
+	glfwSetErrorCallback(error_callback);
 
 
-	//GLenum status = glewInit();
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	window = glfwCreateWindow(640, 640, "Simple example", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
 
-	//if (status != GLEW_OK) {
-	//	LOG("Glew", std::string((char*)glewGetErrorString(status)));
-	//	fprintf(stderr, "%s\n", glewGetErrorString(status));
-	//	char x;
-	//	std::cin >> x;
-	//	exit(EXIT_FAILURE);
-	//}
+
+	GLenum status = glewInit();
+
+	if (status != GLEW_OK) {
+		LOG("Glew", std::string((char*)glewGetErrorString(status)));
+		fprintf(stderr, "%s\n", glewGetErrorString(status));
+		char x;
+		std::cin >> x;
+		exit(EXIT_FAILURE);
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	//Batch2D batch;
 
 	//Texture2D smile(R"(..\resources\smile.png)");
 	//Texture2D square(R"(..\resources\red.png)");
 
-	//while (!glfwWindowShouldClose(window))
-	//{
-	//	batch.Begin();
+	//game.Play();
 
-	//	batch.SetTexture(smile);
+	while (!glfwWindowShouldClose(window))
+	{
+		//batch.Begin();
 
-	//	batch.Draw(glm::vec2(320, 320), glm::vec3(0,0,0), glm::vec2(1,1), glm::vec2(320, 320));
-	//	batch.Draw(glm::vec2(320, 320), glm::vec3(0, 0, 0), glm::vec2(.5f, .5f), glm::vec2(320, 320));
+		//batch.SetTexture(smile);
 
-	//	batch.End();
+		//batch.Draw(glm::vec2(320, 320), glm::vec3(0,0,0), glm::vec2(1,1), glm::vec2(320, 320));
+		//batch.Draw(glm::vec2(320, 320), glm::vec3(0, 0, 0), glm::vec2(.5f, .5f), glm::vec2(320, 320));
 
-	//	glfwSwapBuffers(window);
-	//	glfwPollEvents();
-	//}
-	//glfwDestroyWindow(window);
-	//glfwTerminate();
-	//exit(EXIT_SUCCESS);
+		//batch.End();
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		drawHollowCircle(0, 0, .1f, .45f);
+		drawHollowCircle(-.2, -.2, .1f, -.45f, 5);
+		drawHollowCircle(.2, .2, .1f, .8f, 3);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
